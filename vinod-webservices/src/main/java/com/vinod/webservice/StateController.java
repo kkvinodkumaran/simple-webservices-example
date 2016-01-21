94e3be7a359f5d21e319aff721ed581698ae7c13
Vinod Kariyathungal Kumaran (V) <vinod_kariyathungalku@apple.com>
2016-01-21 08:33:40 -0800

    sd

---
+++ vinod-webservices/src/main/java/com/vinod/webservice/StateController.java
@@ -0,0 +1,33 @@
+package com.vinod.webservice;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("/state")
+public class StateController {
+	@GET
+	@Path("/{param}")
+	@Produces("application/xml")
+	public String getMsg(@PathParam("param") String state) {
+		
+		//if requried use JAXB API to convert our State object to xml
+		String stateDetails = null;
+		if (state.equals("KL")) {
+			stateDetails = "<State><name>KERALA</name><shortname>KL</shortname>"
+					+ "<headq>TRIVANDRUM</headq><language>MALAYALAM</language></State>";
+		} else if (state.equals("KA")) {
+			stateDetails = "<State><name>KARNATAKA</name><shortname>KA</shortname>"
+					+ "<headq>BANGALORE</headq><language>KANNADA</language></State>";
+		} else if (state.equals("TN")) {
+			stateDetails = "<State><name>TAMILNADU</name><shortname>TN</shortname>"
+					+ "<headq>CHENNAI</headq><language>TAMIL</language></State>";
+		} else {
+			stateDetails = "Data not found";
+		}
+		return stateDetails;
+
+	}
+
+}
